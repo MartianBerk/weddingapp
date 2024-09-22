21891e31d787616a8e56144841226f401c8f3950
Martin <baker.bsc@gmail.com>
2024-09-22 09:34:00 +0100

    fix

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,5 +1,5 @@
 // export const BASEURL = "http://127.0.0.1:3000/webapis/wedding";
-export const BASEURL = "https://www.ashleyandmartin.co.uk/webapis/wedding";
+export const BASEURL = "https://ashleyandmartin.co.uk/webapis/wedding";
 
 export const AUTHURL = BASEURL + "/auth";
 export const RSVPURL = BASEURL + "/rsvp";

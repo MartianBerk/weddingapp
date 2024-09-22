12f521ce1c01cf25c2c23124ac5292012a122e06
Martin <baker.bsc@gmail.com>
2024-09-22 11:03:27 +0100

    fix

--- src/components/container/Rsvp.js
+++ src/components/container/Rsvp.js
@@ -240,7 +240,7 @@ class Rsvp extends Component {
         return (
             <div className="rsvp-container">
                 {
-                    this.authenticated !== null && this.guests.length > 0 ? (
+                    this.state.authenticated !== null && this.state.guests.length > 0 ? (
                         (this.state.authenticated === false || this.state.error !== null) ? <Error err={this.state.error} /> : this._renderRsvp()
                     ) : null   
                 }
